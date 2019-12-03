962489cab8e2412c3579ad5d28170011b18e209e
crystal_rim <remember2889@gmail.com>
2019-12-03 23:10:48 +0900

    [INIT] 프로젝트 생성

---
+++ src/main/java/com/ddd/oguogu/OguoguApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.oguogu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OguoguApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OguoguApplication.class, args);
+    }
+
+}
